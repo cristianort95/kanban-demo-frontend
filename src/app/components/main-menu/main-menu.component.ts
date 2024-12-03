671485e19d1abb3acd15953acb1286d2f1c0027f
Cristian Ortiz <cristian.ortizdiaz95@gmail.com>
2024-12-02 21:50:19 -0600

    [ADD]Proyect, add and view

--- src/app/components/main-menu/main-menu.component.ts
+++ src/app/components/main-menu/main-menu.component.ts
@@ -1,10 +1,9 @@
-import {Component, computed, OnInit, signal} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {CustomSidenavComponent} from "../../shared/components/custom-sidenav/custom-sidenav.component";
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
-import {MatSidenav, MatSidenavContainer, MatSidenavContent, MatSidenavModule} from "@angular/material/sidenav";
+import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatToolbar} from "@angular/material/toolbar";
-import {NgxSpinnerComponent} from "ngx-spinner";
 import {RouterOutlet} from "@angular/router";
 import {AuthServiceService} from "../../core/services/AuthService";
 
@@ -15,16 +14,12 @@ import {AuthServiceService} from "../../core/services/AuthService";
   templateUrl: './main-menu.component.html',
   styleUrl: './main-menu.component.sass'
 })
-export class MainMenuComponent implements OnInit {
+export class MainMenuComponent {
   constructor(readonly authService: AuthServiceService) {}
   collapsed = signal(false)
   sidenavWidth = computed(() => this.collapsed() ? '65px' : '225px')
 
   closedSession = () => {
     this.authService.logout()
-  }
-
-  ngOnInit() {
-    console.log('MainMenuComponent cargado.');
   }
 }
--- src/app/shared/components/custom-sidenav/custom-sidenav.component.ts
+++ src/app/shared/components/custom-sidenav/custom-sidenav.component.ts
@@ -1,16 +1,36 @@
 import {Component, computed, Input, OnInit, signal} from '@angular/core';
-import {MatListItem, MatListItemIcon, MatListItemTitle, MatNavList} from "@angular/material/list";
+import {
+  MatListItem,
+  MatListItemAvatar,
+  MatListItemIcon,
+  MatListItemLine, MatListItemMeta,
+  MatListItemTitle,
+  MatNavList
+} from "@angular/material/list";
 import {NgForOf, NgIf} from "@angular/common";
 import {MatIcon, MatIconRegistry} from "@angular/material/icon";
-import {RouterLink, RouterLinkActive} from "@angular/router";
+import {Router, RouterLink, RouterLinkActive} from "@angular/router";
 import {AuthServiceService} from "../../../core/services/AuthService";
 import {validateRules} from "../../../core/guards/auth.guard";
 import {jwtDecode} from "jwt-decode";
+import {CrudService} from "../../../core/services/CrudService";
+import {HttpErrorResponse} from "@angular/common/http";
+import {NgxSpinnerService} from "ngx-spinner";
+import {ASSOCIATION, PROJECT, USERS} from "../../../core/endpoints";
+import {ToastrService} from "ngx-toastr";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {ModalCreateItemComponent} from "../modal-create-item/modal-create-item.component";
+import {FormGroup, Validators} from "@angular/forms";
+import {MatDialog, MatDialogRef} from "@angular/material/dialog";
+import {FieldsFormGroup} from "../../../core/models/FieldsFormGroup";
+import {MatButton, MatFabButton} from "@angular/material/button";
 
 export type MenuItem = {
   icon: string;
   label: string;
-  route?: string;
+  route: string;
+  id: number;
 }
 
 @Component({
@@ -25,41 +45,58 @@ export type MenuItem = {
     MatListItemIcon,
     NgIf,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatFabButton
   ],
   template: `
     <div class="sidenav-container">
-      <div>
-        <div class="sidenav-header">
-          <img [width]="profilePicSize()" [height]="profilePicSize()" src="images/logo.png"  alt="icon"/>
-          <div class="header-text" [class.hide-header-text]="sideNavCollapsed()">
-            <h2>{{ name }}</h2>
-            <p>{{ lastName }}</p>
-          </div>
+      <div class="sidenav-header">
+        <img [width]="profilePicSize()" [height]="profilePicSize()" src="images/logo.png"  alt="icon"/>
+        <div class="header-text" [class.hide-header-text]="sideNavCollapsed()">
+          <h2>{{ name }}</h2>
+          <p>{{ lastName }}</p>
         </div>
+      </div>
+      <div class="sidenav-new-project" [class.hide-sidenav]="sideNavCollapsed()">
         <mat-nav-list [class.hide-sidenav]="sideNavCollapsed()">
           <a
+            (click)="createModal()"
             mat-list-item
-            *ngFor="let item of menuItems()"
-            [routerLink]="item.route"
             routerLinkActive
             #rla="routerLinkActive"
             [activated]="rla.isActive"
           >
             <mat-icon
               class="menu-items"
-              [fontSet]="
-            rla.isActive ? 'material-icons' : 'material-icons-outlined'
-            "
+              [fontSet]="rla.isActive ? 'material-icons' : 'material-icons-outlined'"
               [class.selected-menu-item]="rla.isActive"
               matListItemIcon
-            >{{item.icon}}</mat-icon>
-            <span matListItemTitle *ngIf="!sideNavCollapsed()" >
-                {{item.label}}
-            </span>
+            >add</mat-icon>
+            <span matListItemTitle *ngIf="!sideNavCollapsed()">Crear Proyecto</span>
           </a>
         </mat-nav-list>
       </div>
+      <mat-nav-list class="sidenav-body" [class.hide-sidenav]="sideNavCollapsed()">
+        <div *ngFor="let item of menuItems()">
+          <a
+            mat-list-item
+            routerLinkActive
+            #rla="routerLinkActive"
+            [activated]="rla.isActive"
+            [routerLink]="item.route"
+          >
+            <mat-icon
+              class="menu-items"
+              [fontSet]="rla.isActive ? 'material-icons' : 'material-icons-outlined'"
+              [class.selected-menu-item]="rla.isActive"
+              matListItemIcon
+            >{{item.icon}}</mat-icon>
+            <span matListItemTitle *ngIf="!sideNavCollapsed()">
+                {{item.label}}
+            </span>
+          </a>
+        </div>
+      </mat-nav-list>
 
       <!-- Menú inferior -->
       <div class="bottom-menu">
@@ -86,61 +123,83 @@ export type MenuItem = {
   `,
   styles: [
     `
-      :host *
-        transition: all 500ms ease-in-out
+    :host *
+      transition: all 500ms ease-in-out
 
-      .sidenav-container
-        display: flex
-        flex-direction: column
-        height: 100%
-        justify-content: space-between
+    .sidenav-container
+      display: flex
+      flex-direction: column
+      height: 100%
+      justify-content: space-between
 
-      .sidenav-header
-        padding-top: 24px
-        text-align: center
-        > img
-          border-radius: 100%
-          object-fit: cover
-          margin-bottom: 10px
+    .sidenav-new-project
+      text-align: center
 
-        .header-text
-          height: 3rem
-          > h2
-            margin: 0
-            font-size: 1rem
-            line-height: 1.5rem
+    .sidenav-header
+      padding-top: 24px
+      text-align: center
+      > img
+        border-radius: 100%
+        object-fit: cover
+        margin-bottom: 10px
 
-          > p
-            margin-top: 0
-            font-size: 0.8rem
-    .hide-header-text
-      transition: all 200ms ease-in-out!important
-      opacity: 0
-      height: 0!important
-      overflow: hidden
-    .hide-sidenav
-      a
-        width: 56px
-        margin-left: 5px
-        margin-right: 5px
-      .menu-items
-        border-left: 0px solid
-      .selected-menu-item
-        border-left: 4px solid
+      .header-text
+        height: 3rem
+        > h2
+          margin: 0
+          font-size: 1rem
+          line-height: 1.5rem
 
+        > p
+          margin-top: 0
+          font-size: 0.8rem
+  .hide-header-text
+    transition: all 200ms ease-in-out!important
+    opacity: 0
+    height: 0!important
+    overflow: hidden
+  .sidenav-body
+    height: 100%
+    overflow-x: auto
+    &::-webkit-scrollbar
+      width: 5px
+    &::-webkit-scrollbar-thumb
+      background: gray
+      border-radius: 50px
+
+    &::-webkit-scrollbar-track
+      background-color: black
+      border-radius: 50px
+
+  .hide-sidenav
+    a
+      width: 56px
+      margin-left: 5px
+      margin-right: 5px
     .menu-items
-      border-left: 4px solid
-      border-color: rgb(0,0,0,0)
+      border-left: 0px solid
     .selected-menu-item
-      border-color: var(--primary-color)
+      border-left: 4px solid
+
+  .menu-items
+    border-left: 4px solid
+    border-color: rgb(0,0,0,0)
+  .selected-menu-item
+    border-color: var(--primary-color)
     `
   ]
 })
-export class CustomSidenavComponent {
+export class CustomSidenavComponent implements OnInit {
   name = ''
   lastName = ''
 
-  constructor(readonly authService: AuthServiceService) {
+  constructor(
+    readonly authService: AuthServiceService,
+    readonly service: CrudService,
+    readonly spinner: NgxSpinnerService,
+    readonly toastr: ToastrService,
+    readonly dialog: MatDialog,
+  ) {
     const isAuthenticated = localStorage.getItem('authToken');
     if (isAuthenticated) {
       const decoded: any = jwtDecode(isAuthenticated);
@@ -149,35 +208,73 @@ export class CustomSidenavComponent {
     }
   }
 
+  dialogUpdate?: MatDialogRef<ModalCreateItemComponent>
   sideNavCollapsed = signal(false)
   @Input() set collapsed(value: boolean) {
     this.sideNavCollapsed.set(value)
   }
-  menuItems = signal<MenuItem[]>([
-    {
-      icon: 'account_balance',
-      label: 'Asociaciones',
-      route: 'association',
-    },
-    {
-      icon: 'manage_accounts',
-      label: 'Users',
-      route: 'users',
-    },
-    {
-      icon: 'contact_mail',
-      label: 'Socios',
-      route: 'partners',
-    },
-    {
-      icon: 'forest',
-      label: 'Parcelas',
-      route: 'plots',
-    }
-  ])
+  menuItems = signal<MenuItem[]>([])
   profilePicSize = computed(()=>this.sideNavCollapsed() ? '50' : '100')
+  fields: FieldsFormGroup[] = [
+    {name: "name", label: "Nombre", type: "input", validator: [Validators.required]},
+    {name: "description", label: "Descripcion", type: "input", validator: [Validators.required]},
+  ]
 
   closedSession = () => {
     this.authService.logout()
+  }
+
+  ngOnInit(): void {
+    this.getProjects().then()
+  }
+
+  async getProjects() {
+    this.spinner.show("getProject").then()
+    this.service.get(PROJECT+"?limit=200&page=1&relations=project&project=name,description").subscribe((response: any) => {
+      const data = response.data.map((item: any) => {
+        return {
+          icon:"work",
+          label:item.project.name,
+          route:"projects/"+item.projectId,
+          id:item.projectId,
+        }
+      })
+      this.menuItems.set(data)
+      this.spinner.hide('getProject').then()
+    }, (error: HttpErrorResponse) => {
+      this.spinner.hide('getProject').then()
+    });
+  }
+
+  removeProject(id: number) {
+    this.spinner.show("deleteProject").then()
+    this.service.delete(`${PROJECT}/${id}`).subscribe((response: any) => {
+
+      this.spinner.hide('deleteProject').then()
+      this.toastr.success("Proyecto Eliminado!");
+    }, (error: HttpErrorResponse) => {
+      this.toastr.error("Error intente de nuevo o revise los datos relacionados al proyecto!");
+      this.spinner.hide('deleteProject').then()
+    });
+  }
+
+
+  async createModal() {
+    this.dialogUpdate = this.dialog.open(ModalCreateItemComponent, {
+      width: '90%',
+      data: this.fields
+    })
+    this.dialogUpdate.afterClosed().subscribe((result: FormGroup) => {
+      if (result) {
+        this.spinner.show("create")
+        this.service.post(PROJECT, result.value).subscribe((response: any) => {
+          this.spinner.hide("create")
+          this.getProjects().then()
+          this.toastr.success("Proyecto Creado!");
+        }, (error: HttpErrorResponse) => {
+          this.spinner.hide("create")
+        })
+      }
+    })
   }
 }
